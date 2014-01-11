98d7b6d2d9365af8e14077e31565ae95433527aa
Alexia McKenzie <pandapprentice@gmail.com>
2014-01-11 11:12:25 -0800

    validation visuals

--- validate.js
+++ validate.js
@@ -1,15 +1,19 @@
 function validateUser() {
   if (!$('.usernameField').val()) {
+    $('.usernameField').addClass('success');
     console.log("Username Doesn't Work");
   } else {
+    $('.usernameField').addClass('error');
     console.log("Username Works: " + $('.usernameField').val());
   }
 }
 
 function validatePassword() {
   if ($('.passwordField').val().match(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[!@#$%^&*()]).{6,}$/)) {
+    $('.passwordField').addClass('success');
     console.log("Password Works: " + $('.passwordField').val());
   } else {
+    $('.passwordField').addClass('error');
     console.log("Password Doesn't Work");
   }
 }
